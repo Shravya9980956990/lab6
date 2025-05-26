2eac9a1dd020f4e90159b4c6d70543c5b8bb91a9
ksit <ksit@matlab9>
2025-05-26 09:01:15 +0530

    initial commit

---
+++ lab61/src/main/java/lab61/hello.java
@@ -0,0 +1,11 @@
+package lab61;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a maven project");
+
+	}
+
+}
